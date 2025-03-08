bf2bf1b659ff81f08c169b290ad0a669e8e17608
16sumanshiroy <112249799+16sumanshiroy@users.noreply.github.com>
2025-03-08 20:03:03 +0530

    script.js

---
+++ static/script.js
@@ -0,0 +1,6 @@
+// JavaScript placeholder file
+// This file ensures JavaScript is detected in GitHub language stats
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Hostel MiniSite Loaded!");
+});
